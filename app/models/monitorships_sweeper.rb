2df738ec2f655e8442f21bce0b8e6c85235fc584
Vladimir Meremyanin <vladimir@meremyanin.com>
2011-01-31 12:51:11 +0800

    fixed deprecation RAILS_ROOT -> Rails.root

--- app/models/monitorships_sweeper.rb
+++ app/models/monitorships_sweeper.rb
@@ -2,7 +2,7 @@ class MonitorshipsSweeper < ActionController::Caching::Sweeper
   observe Monitorship
   
   def after_save(monitorship)
-    FileUtils.rm_rf File.join(RAILS_ROOT, 'public', 'users', monitorship.user_id.to_s)
+    FileUtils.rm_rf File.join(Rails.root, 'public', 'users', monitorship.user_id.to_s)
   end
   
   alias_method :after_destroy, :after_save
