d435ad5bd163863fc74b32a5dd5c00b77cff7cec
Vladimir Meremyanin <vladimir@meremyanin.com>
2011-01-31 11:45:53 +0800

    updated to rails 3 (rails new .)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlteredBeast::Application.config.secret_token = 'a52143001ceb82c5e2f94fefcba0a117b6c3653b711f26fff08a547805afc1e596ede8583ffd0c6a4a0945653352570c4ec922cd69ff15ec27282a7bc5053fee'
