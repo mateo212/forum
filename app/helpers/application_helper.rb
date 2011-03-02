ff8c8e7c594e452825d86175829392fb1eade1a5
Gabriel Mazetto <brodock@gmail.com>
2011-03-02 04:10:31 -0300

    Updated I18n files and default localization strings to match new I18n interpolation syntax: From {{var}} to %{var}

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,8 +21,8 @@ module ApplicationHelper
 
   def search_posts_title
     (params[:q].blank? ? I18n.t('txt.recent_posts', :default => 'Recent Posts') : I18n.t('txt.searching_for', :default => 'Searching for') + " '#{h params[:q]}'").tap do |title|
-      title << " " + I18n.t('txt.by_user', :default => 'by {{user}}', :user => h(@user.display_name)) if @user
-      title << " " + I18n.t('txt.in_forum', :default => 'in {{forum}}', :forum => h(@forum.name)) if @forum
+      title << " " + I18n.t('txt.by_user', :default => 'by %{user}', :user => h(@user.display_name)) if @user
+      title << " " + I18n.t('txt.in_forum', :default => 'in %{forum}', :forum => h(@forum.name)) if @forum
     end
   end
 
